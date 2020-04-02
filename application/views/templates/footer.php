4801dc94534d7e7a42999a86fd7fbdf7b3a584a6
Krizza Bullecer <krizzabullecer@gmail.com>
2020-04-02 15:34:56 +0800

    add vue

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,7 +1,14 @@
 </div>
-<script src="<?php echo asset_url(); ?>js/jquery.min.js"></script>
-<script src="<?php echo asset_url(); ?>js/popper.min.js"></script>
-<script src="<?php echo asset_url(); ?>js/bootstrap.min.js"></script>
+</div>
+<script>
+    var app = new Vue({
+        el: '#app',
+        data: {
+            seen: 'hello'
+        }
+    })
+</script>
+
 </body>
 
 </html>
