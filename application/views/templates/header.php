24b8583e01c7a8eaf959d0ec2f472b14cede3af5
Krizza Bullecer <krizzabullecer@gmail.com>
2020-04-02 15:56:52 +0800

    add new styles for nav

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,35 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://bootswatch.com/4/minty/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo asset_url(); ?>css/grid.min.css">
+    <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:600|IBM+Plex+Sans|Playfair+Display:500&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/134fbda4cd.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <link rel="stylesheet" href="<?php echo asset_url(); ?>css/style.css">
     <title>Gigs Tracker</title>
 </head>
 
 <body>
-    <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-primary">
-        <div class="container">
-            <a class="navbar-brand" href="<?= base_url() ?>">Gig List</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarColor01">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="<?= base_url() ?>">Gigs</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Musicians</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Notifications</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">User</a>
-                    </li>
-                </ul>
+    <div id="app">
+        <nav class="container d-flex align-items-center justify-content-between">
+            <div class="brand">
+                <a href="<?= base_url() ?>">Gigs Tracker</a>
             </div>
-        </div>
-    </nav>
+            <ul class="d-flex">
+                <li class="p-2"><a class="<?= $this->router->class === 'gigs' ? 'active' : '' ?>" href="<?= base_url() ?>">Gigs</a></li>
+                <li class="p-2"><a class="<?= $this->router->class === 'musicians' ? 'active' : '' ?>" href="<?= base_url() ?>musicians">Musicians</a></li>
+                <li class="p-2"><a class="<?= $this->router->class === 'notifications' ? 'active' : '' ?>" href="<?= base_url() ?>notifications">Activity</a></li>
+                <li class="p-2"><a class="<?= $this->router->class === 'settings' ? 'active' : '' ?>" href="<?= base_url() ?>settings">Settings</a></li>
+            </ul>
+            <a class="web-logout" href="<?= base_url() ?>/logout">Logout</a>
+        </nav>
 
-    <div class="container">
+        <div class="container">
