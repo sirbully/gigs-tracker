daa1aaeea90d414a0a6e9beb0d92378ba2c8e6c0
Krizza Bullecer <krizzabullecer@gmail.com>
2020-04-03 00:14:27 +0800

    complete design and text

--- application/views/templates/notfound.php
+++ application/views/templates/notfound.php
@@ -1,1 +1,3 @@
-<h1>404 Page Not Found</h1>
+<h3>OH NO! ERROR 404</h3>
+<h1>Page Not Found</h1>
+<p class="mt-3">This page doesn't exist! You might have a typo on the link<br />or the page has been removed!</p>
