d7864908b0c739ee304e775f6fb3990609ee9a51
Krizza Bullecer <krizzabullecer@gmail.com>
2020-04-02 15:57:01 +0800

    add 404 page

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,5 +3,5 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 $route['default_controller'] = 'gigs/index';
 $route['assets/(:any)'] = 'assets/$1';
-$route['404_override'] = '';
+$route['404_override'] = 'notfound/index';
 $route['translate_uri_dashes'] = FALSE;
