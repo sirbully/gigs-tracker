e63924b91657e323a44d983da32d8891cc66f1db
Krizza Bullecer <krizzabullecer@gmail.com>
2020-04-03 13:31:42 +0800

    update view

--- application/views/gigs/view.php
+++ application/views/gigs/view.php
@@ -1,2 +1,59 @@
 <h3>Detailed worksheet for</h3>
-<h1><?= date_format(new DateTime($gig['date']), 'D M j, Y') ?></h1>
+<h1><?= date_format(new DateTime($gig['date']), 'D M jS, Y') ?></h1>
+
+<div id="table">
+    <div class="container">
+        <div id="thead" class="row align-items-center justify-content-between">
+            <p class="d-flex align-items-center">
+                <i class="fas fa-info-circle"></i>
+                <span>Basic Info</span>
+            </p>
+            <div id="view-action">
+                <a href="<?= base_url() . 'gigs' ?>">
+                    <i class="fas fa-arrow-left"></i>
+                    <small>Go Back</small>
+                </a>
+                <a>
+                    <i class="far fa-calendar-plus"></i>
+                    <small>Calendar</small>
+                </a>
+                <a>
+                    <i class="far fa-edit"></i>
+                    <small>Edit</small>
+                </a>
+                <a>
+                    <i class="far fa-times-circle"></i>
+                    <small>Cancel</small>
+                </a>
+            </div>
+        </div>
+        <div class="row trow">
+            <p>Booking Status</p>
+            <p><?= $gig['status'] === '1' ? 'Confirmed' : 'Canceled' ?></p>
+        </div>
+        <div class="row trow">
+            <p>Date and Time</p>
+            <p><?= date_format(new DateTime($gig['date']), 'l F j, Y, g:iA') ?></p>
+        </div>
+        <div class="row trow">
+            <p>Type</p>
+            <p><?= $gig['type'] ?></p>
+        </div>
+        <div class="row trow">
+            <p>Location</p>
+            <p><?= $gig['location'] ?></p>
+        </div>
+        <div class="row trow">
+            <p>Client</p>
+            <p><?= $gig['client'] ?></p>
+        </div>
+        <div class="row trow">
+            <p>Dresscode</p>
+            <p><?= $gig['dress'] ?></p>
+        </div>
+        <div class="row trow">
+            <p>Pay</p>
+            <p><?= $gig['pay'] ?></p>
+        </div>
+    </div>
+</div>
