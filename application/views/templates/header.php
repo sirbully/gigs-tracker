78c206258f2074ec981a74238fc9fff04675094b
Krizza Bullecer <krizzabullecer@gmail.com>
2020-04-03 12:05:24 +0800

    updated font and website brand

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo asset_url(); ?>css/grid.min.css">
-    <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:600|IBM+Plex+Sans|Playfair+Display:400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:600|IBM+Plex+Sans|Playfair+Display&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/134fbda4cd.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
     <link rel="stylesheet" href="<?php echo asset_url(); ?>css/style.css">
@@ -16,7 +16,7 @@
     <div id="app">
         <nav class="container d-flex align-items-center justify-content-between">
             <div class="brand">
-                <a href="<?= base_url() ?>">Gigs Tracker</a>
+                <a href="<?= base_url() ?>">Mister Shakes</a>
             </div>
             <ul class="d-flex">
                 <li class="p-2"><a class="<?= $this->router->class === 'gigs' ? 'active' : '' ?>" href="<?= base_url() ?>">Gigs</a></li>
