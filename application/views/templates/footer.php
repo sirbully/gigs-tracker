cf55a7e1fe79baf974e42c6988146077704f7d45
Krizza Bullecer <krizzabullecer@gmail.com>
2020-04-03 15:29:07 +0800

    add datepicker and currency formatter

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,5 +1,7 @@
 </div>
 </div>
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script src="https://unpkg.com/vuejs-datepicker"></script>
 <script>
     var app = new Vue({
         el: '#app',
@@ -8,8 +10,21 @@
             let params = new URLSearchParams(uri);
             this.page = params.get("page");
         },
+        components: {
+            vuejsDatepicker
+        },
         data: {
-            page: null
+            page: null,
+            form_success: '<?= $success === false ? 'false' : 'true' ?>'
+        },
+        methods: {
+            toCurrency(num) {
+                const toNum = Number(num)
+                return toNum.toLocaleString(undefined, {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2
+                })
+            }
         }
     })
 </script>
