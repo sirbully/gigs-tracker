97a7da022cfbe47647865dfb1cc1070ee5c9a7b2
Krizza Bullecer <krizzabullecer@gmail.com>
2020-04-03 15:28:19 +0800

    add create route for gigs

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,6 +3,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 $route['assets/(:any)'] = 'assets/$1';
 $route['default_controller'] = 'gigs/index';
+$route['gigs/create'] = 'gigs/create';
 $route['gigs/(:any)'] = 'gigs/view/$1';
 $route['404_override'] = 'notfound';
 $route['translate_uri_dashes'] = FALSE;
