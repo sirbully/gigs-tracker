7150617d45eb879d443d89d00d14b9b0444116bd
Krizza Bullecer <krizzabullecer@gmail.com>
2020-04-03 15:29:27 +0800

    transfer scripts to footer

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,7 +7,6 @@
     <link rel="stylesheet" href="<?php echo asset_url(); ?>css/grid.min.css">
     <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:600|IBM+Plex+Sans|Playfair+Display&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/134fbda4cd.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
     <link rel="stylesheet" href="<?php echo asset_url(); ?>css/style.css">
     <title>Gigs Tracker</title>
 </head>
