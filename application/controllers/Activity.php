0a14a4f4eb7c7a648b264e6a3125fe548997b61a
Krizza Bullecer <krizzabullecer@gmail.com>
2020-04-04 22:28:00 +0800

    add index and load views

--- application/controllers/Activity.php
+++ application/controllers/Activity.php
@@ -13,6 +13,13 @@ class Activity extends CI_Controller
 
     public function index()
     {
+        if ($this->session->userdata('isAdmin')) {
+            // $data['activity'] = $this->gig_model->get_activity();
+        } else {
+            // $data['activity'] = $this->gig_model->get_activity_musician($this->session->userdata('user_id'));
+        }
+        // print_r($data['activity']);
+
         $this->load->view('templates/header');
         $this->load->view('templates/activity');
         $this->load->view('templates/footer');
