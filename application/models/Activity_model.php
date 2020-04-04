9d2aabf1e090098c56b1970921b6e094b07a72f5
Krizza Bullecer <krizzabullecer@gmail.com>
2020-04-04 22:29:05 +0800

    init model

---
+++ application/models/Activity_model.php
@@ -0,0 +1,18 @@
+<?php
+class Activity_model extends CI_Model
+{
+    public function __construct()
+    {
+        $this->load->database();
+    }
+
+    public function get_activity()
+    {
+        //
+    }
+
+    public function get_activity_musician($id)
+    {
+        // code here
+    }
+}
