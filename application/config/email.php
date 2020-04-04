9fa285485f8669b71efda3f932d620dad4e0d088
Krizza Bullecer <krizzabullecer@gmail.com>
2020-04-05 01:15:11 +0800

    init email config

---
+++ application/config/email.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+$config = array(
+    'protocol' => 'smtp',
+    'smtp_host' => 'mail3.gridhost.co.uk',
+    'smtp_port' => 465,
+    'smtp_user' => 'notify@elliottrooney.co.uk',
+    'smtp_pass' => 't8WcvmnF2pT?T9X9',
+    'mailtype' => 'html',
+);
