960f7035b5040487f90f53cd5e9d464892f6d899
Krizza Bullecer <krizzabullecer@gmail.com>
2020-04-05 00:14:16 +0800

    complete functionality

--- application/controllers/Settings.php
+++ application/controllers/Settings.php
@@ -2,7 +2,7 @@
 
 class Settings extends CI_Controller
 {
-    function __construct()
+    public function __construct()
     {
         parent::__construct();
 
@@ -13,8 +13,68 @@ class Settings extends CI_Controller
 
     public function index()
     {
+        $data['user'] = $this->setting_model->get_user_info();
+
         $this->load->view('templates/header');
-        $this->load->view('templates/settings');
+        $this->load->view('templates/settings', $data);
         $this->load->view('templates/footer');
+    }
+
+    public function update_name()
+    {
+        $data['user'] = $this->setting_model->get_user_info();
+        $this->form_validation->set_rules('name', 'Name', 'required');
+
+        if ($this->form_validation->run() === FALSE) {
+            $this->load->view('templates/header');
+            $this->load->view('templates/settings', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $this->setting_model->update(array('name' => $this->input->post('name')));
+            $this->session->set_flashdata('settings', "You were logged out for updating your settings.");
+            redirect("members/logout");
+        }
+    }
+
+    public function update_email()
+    {
+        $data['user'] = $this->setting_model->get_user_info();
+        $this->form_validation->set_rules('email', 'Email', 'required|callback_check_email');
+
+        if ($this->form_validation->run() === FALSE) {
+            $this->load->view('templates/header');
+            $this->load->view('templates/settings', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $this->setting_model->update(array('email' => $this->input->post('email')));
+            $this->session->set_flashdata('settings', "You were logged out for updating your settings.");
+            redirect("members/logout");
+        }
+    }
+
+    public function update_password()
+    {
+        $data['user'] = $this->setting_model->get_user_info();
+        $this->form_validation->set_rules('password', 'Password', 'required');
+
+        if ($this->form_validation->run() === FALSE) {
+            $this->load->view('templates/header');
+            $this->load->view('templates/settings', $data);
+            $this->load->view('templates/footer');
+        } else {
+            $this->setting_model->update(array('email' => $this->input->post('password')));
+            $this->session->set_flashdata('settings', "You were logged out for updating your settings.");
+            redirect("members/logout");
+        }
+    }
+
+    public function check_email($email)
+    {
+        $this->form_validation->set_message('check_email', 'This email has already been registered.');
+        if ($this->musician_model->check_email($email)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
