6e95e655295d813ca533c3eae439225fa24b5287
Krizza Bullecer <krizzabullecer@gmail.com>
2020-04-06 02:18:11 +0800

    complete activity

--- application/models/Activity_model.php
+++ application/models/Activity_model.php
@@ -8,11 +8,40 @@ class Activity_model extends CI_Model
 
     public function get_activity()
     {
-        //
+        $query = $this->db->get('notifications');
+        return $query->result_array();
     }
 
     public function get_activity_musician($id)
     {
-        // code here
+        $query = $this->db->get('notifications', array('user_id' => $id));
+        return $query->result_array();
+    }
+
+    public function new_user($name)
+    {
+        $notif = array(
+            'message' => 'Added new user: ' . $name,
+            'user_id' => $this->session->userdata('user_id')
+        );
+
+        return $this->db->insert('notifications', $notif);
+    }
+
+    public function cancel_gig($notif)
+    {
+        return $this->db->insert('notifications', $notif);
+    }
+
+    public function decide_status($id, $status)
+    {
+        $new_status = $status == 1 ? 'accepted' : 'rejected';
+        $query = $this->gig_model->get_gigs($id);
+        $notif = array(
+            'message' => 'You have ' . $new_status . ' the following gig: [' . date_format(new DateTime($query['date']), 'M jS, Y') . '] ' . $query['type'] . '',
+            'user_id' => $this->session->userdata('user_id')
+        );
+
+        return $this->db->insert('notifications', $notif);
     }
 }
