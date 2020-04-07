f3389618a1a9a5f32267a5e79da8f9c656f1c49e
Krizza Bullecer <krizzabullecer@gmail.com>
2020-04-07 23:13:03 +0800

    add a config file for upload

---
+++ application/config/upload.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+$config = array(
+    'upload_path' => './uploads/',
+    'allowed_types' => 'jpg|png|pdf|doc|docx',
+    'file_ext_tolower' => TRUE,
+    'encrypt_name' => TRUE
+);
